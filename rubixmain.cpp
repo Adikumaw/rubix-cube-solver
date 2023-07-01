73ec56e77108dbe263bf5e48ffba305531a7149e
all~father <kumawataditya105@gmail.com>
2023-07-01 17:48:42 +0530

    removed unusable code from the cpp

--- rubixmain.cpp
+++ rubixmain.cpp
@@ -5,39 +5,13 @@
 #include <thread>
 #include "makeCubie.h"
 
-enum side
-{
-    face,
-    back,
-    left,
-    right,
-    top,
-    bottom,
-    mid,
-    equator,
-    stand
-
-};
-
-#define F newCube.sideRotate(side::face, "clk");
-#define L newCube.sideRotate(side::left, "clk");
-#define R newCube.sideRotate(side::right, "clk");
-#define U newCube.sideRotate(side::top, "clk");
-#define D newCube.sideRotate(side::bottom, "clk");
-#define B newCube.sideRotate(side::back, "clk");
-#define FP newCube.sideRotate(side::face, "antclk");
-#define LP newCube.sideRotate(side::left, "antclk");
-#define RP newCube.sideRotate(side::right, "antclk");
-#define UP newCube.sideRotate(side::top, "antclk");
-#define DP newCube.sideRotate(side::bottom, "antclk");
-#define BP newCube.sideRotate(side::back, "antclk");
-
 using namespace std;
 
 int main()
 {
     makeCubie newCube;
     newCube.setcube();
+    // Start the timer
     auto startTime = std::chrono::high_resolution_clock::now();
     // CALLING CROSS_SOLVER..........
     newCube.crossSolver();
@@ -65,6 +39,7 @@ int main()
     // chrono::sleep_until(system_clock::now() + seconds(1));
     // using namespace std::chrono; // nanoseconds, system_clock, seconds
     // this_thread::sleep_for(chrono::nanoseconds(60000000));
+
     // Stop the timer
     auto endTime = std::chrono::high_resolution_clock::now();
 
