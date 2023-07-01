1d74ae0462dd5fc910ecaaebec4ecd4ce4f4797a
all~father <kumawataditya105@gmail.com>
2023-07-01 17:45:51 +0530

    added timer counter and removed unusable code

--- rubixmain.cpp
+++ rubixmain.cpp
@@ -5,46 +5,19 @@
 #include <thread>
 #include "makeCubie.h"
 
-enum side
-{
-    face,
-    back,
-    left,
-    right,
-    top,
-    bottom,
-    mid,
-    equator,
-    stand
-
-};
-
-#define F newCube.sideRotate(side::face, "clk");
-#define L newCube.sideRotate(side::left, "clk");
-#define R newCube.sideRotate(side::right, "clk");
-#define U newCube.sideRotate(side::top, "clk");
-#define D newCube.sideRotate(side::bottom, "clk");
-#define B newCube.sideRotate(side::back, "clk");
-#define FP newCube.sideRotate(side::face, "antclk");
-#define LP newCube.sideRotate(side::left, "antclk");
-#define RP newCube.sideRotate(side::right, "antclk");
-#define UP newCube.sideRotate(side::top, "antclk");
-#define DP newCube.sideRotate(side::bottom, "antclk");
-#define BP newCube.sideRotate(side::back, "antclk");
-
 using namespace std;
 
 int main()
 {
     makeCubie newCube;
     newCube.setcube();
+    // Start the timer
     auto startTime = std::chrono::high_resolution_clock::now();
     // CALLING CROSS_SOLVER..........
     newCube.crossSolver();
     cout << "---------------------------------------------------------------" << endl;
     cout << "CROSS SOLUTION:  ";
     newCube.getCrossSolution();
-    newCube.getcube();
     cout << "\n---------------------------------------------------------------" << endl;
     // CALLING F2L_SOLVER..........
     newCube.shortestF2LSolver();
