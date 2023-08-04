504399cfe0762758f723b1370f8ecedaab32f1e3
all~father <kumawataditya105@gmail.com>
2023-08-05 00:49:39 +0530

    defined font colors, styles, and backgroud ascii for console

---
+++ colors.h
@@ -0,0 +1,49 @@
+#ifndef _COLORS_H_
+#define _COLORS_H_
+
+// FONT COLORS...
+#define BLACK "\033[38;5;0m"
+#define RED "\033[38;5;1m"
+#define GREEN "\033[38;5;2m"
+#define YELLOW "\033[38;5;3m"
+#define BLUE "\033[38;5;4m"
+#define MAGENTA "\033[38;5;5m"
+#define CYAN "\033[38;5;6m"
+#define WHITE "\033[38;5;7m"
+#define B_BLACK "\033[38;5;8m"
+#define B_RED "\033[38;5;9m"
+#define B_GREEN "\033[38;5;10m"
+#define B_YELLOW "\033[38;5;11m"
+#define B_BLUE "\033[38;5;12m"
+#define B_MAGENTA "\033[38;5;13m"
+#define B_CYAN "\033[38;5;14m"
+#define B_WHITE "\033[38;5;15m"
+// BACKGROUD COLORS...
+#define BG_BLACK "\033[48;5;0m"
+#define BG_RED "\033[48;5;1m"
+#define BG_GREEN "\033[48;5;2m"
+#define BG_YELLOW "\033[48;5;3m"
+#define BG_BLUE "\033[48;5;4m"
+#define BG_MAGENTA "\033[48;5;5m"
+#define BG_CYAN "\033[48;5;6m"
+#define BG_WHITE "\033[48;5;7m"
+#define BG_B_BLACK "\033[48;5;8m"
+#define BG_B_RED "\033[48;5;9m"
+#define BG_B_GREEN "\033[48;5;10m"
+#define BG_B_YELLOW "\033[48;5;11m"
+#define BG_B_BLUE "\033[48;5;12m"
+#define BG_B_MAGENTA "\033[48;5;13m"
+#define BG_B_CYAN "\033[48;5;14m"
+#define BG_B_WHITE "\033[48;5;15m"
+// text styling..
+#define BOLD "\033[1m"
+#define FAINT "\033[2m"
+#define ITALIC "\033[3m"
+#define UNDERLINE "\033[4m"
+#define BLINKING "\033[5m"
+#define REVERSE "\033[7m"
+#define CONCEAL "\033[8m"
+// set to default..
+#define DEFAULT "\033[0m"
+
+#endif //_COLORS_H_
